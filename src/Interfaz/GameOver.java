99a76eb2715e364e6f7b52c563b3185957df4746
Mary <estresha@hotmail.com.ar>
2024-09-17 00:21:22 -0300

    14 commit

--- src/Interfaz/GameOver.java
+++ src/Interfaz/GameOver.java
@@ -109,6 +109,7 @@ public class GameOver {
 //-----------------------------------------------------------------------------------------------------------------------------------	
 		
 	public static void main(String[] args) {
+		
 			EventQueue.invokeLater(new Runnable() {
 				public void run() {
 					try {
